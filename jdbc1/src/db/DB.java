bc7387b53f3c9d925fbf297c3d3be559885cd1f5
Júlio César Soares de Almeida <60848753+jccesar22@users.noreply.github.com>
2021-10-25 23:06:52 -0300

    banco de dados jdbc

--- jdbc1/src/db/DB.java
+++ jdbc1/src/db/DB.java
@@ -1,5 +1,51 @@
 package db;
 
-public class DB {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
+public class DB {
+	//colocando a conexão como null "inicializando ela"
+	private static Connection conn = null;
+	//fazendo um teste de conexão com o banco de dados
+	public static Connection getConnection() {
+		if (conn == null) {
+			try {
+			Properties props = loadProperties();
+			String url = props.getProperty("dburl");
+			conn = DriverManager.getConnection(url, props);
+			}
+			catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		return conn;
+	}
+	
+	//fechando a cone~xao com o banco de dados
+	public static void closeConnection() {
+		if(conn != null) {
+			try {
+			conn.close();
+			}
+			catch(SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	//abrindo a conexão com o banco de dados
+	private static Properties loadProperties() {
+		try (FileInputStream fs = new FileInputStream("db.properties")){
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		}
+		catch (IOException e){
+			throw new DbException(e.getMessage());
+		}
+	}
 }
