376cb39dc0286f5fe43ebfae7a56bde890e055e8
Júlio César Soares de Almeida <60848753+jccesar22@users.noreply.github.com>
2021-10-31 10:05:05 -0300

    commit

---
+++ jdbc3/src/db/DB.java
@@ -0,0 +1,76 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class DB {
+	//colocando a conexão como null "inicializando ela"
+	private static Connection conn = null;
+	//fazendo um teste de conexão com o banco de dados
+	public static Connection getConnection() {
+		if (conn == null) {
+			try {
+			Properties props = loadProperties();
+			String url = props.getProperty("dburl");
+			conn = DriverManager.getConnection(url, props);
+			}
+			catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		return conn;
+	}
+	
+	//fechando a cone~xao com o banco de dados
+	public static void closeConnection() {
+		if(conn != null) {
+			try {
+			conn.close();
+			}
+			catch(SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	//abrindo a conexão com o banco de dados
+	private static Properties loadProperties() {
+		try (FileInputStream fs = new FileInputStream("db.properties")){
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		}
+		catch (IOException e){
+			throw new DbException(e.getMessage());
+		}
+	}
+	
+	
+	public static void closlStatement(Statement st) {
+		if(st !=  null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	public static void closlResultSet(ResultSet rs) {
+		if(rs !=  null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+}
