ac301109d82f0b8a9fad02e00e0343887dc47bb8
Júlio César Soares de Almeida <60848753+jccesar22@users.noreply.github.com>
2021-11-02 15:01:16 -0300

    COMMIT

--- jdbc3/src/application/Program.java
+++ jdbc3/src/application/Program.java
@@ -2,7 +2,9 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -18,28 +20,39 @@ public class Program {
 		PreparedStatement st = null;
 		try {
 			conn = DB.getConnection();
+			/*
 			st = conn.prepareStatement(
 					"INSERT INTO SELLER"
 				   +"(Name, Email, BirthDate, BaseSalary, DepartmentId) "
 				   +"VALUES "
-				   +"(?, ?, ?, ?, ?)");
+				   +"(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
 			st.setString(1, "Call  Purpple");
 			st.setString(2, "Call@gmail.com");
 			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1955").getTime()));
 			st.setDouble(4, 3000.00);
 			st.setInt(5, 4);
 			//para saber quantas linhas foram editadas
+			 * */
+			st = conn.prepareStatement("insert into department (Name) values ('D1'),('D2')", Statement.RETURN_GENERATED_KEYS);
+			
 			int rowsAffected = st.executeUpdate();
 			
 			
-			System.out.println("Done! Rows affected: "  + rowsAffected);
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				while(rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Done Id = "+ id);
+				}
+			}
+			else {
+				System.out.println("no rown affected");
+			}
 		}
 		catch(SQLException e){
 			e.printStackTrace();
 		}
-		catch(ParseException e) {
-			e.printStackTrace();
-		}
+		
 		finally {
 			DB.closlStatement(st);
 			DB.closeConnection();
