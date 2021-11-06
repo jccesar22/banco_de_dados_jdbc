f97bd57197d24c1a617985c839d0899664dc50ae
Júlio César Soares de Almeida <60848753+jccesar22@users.noreply.github.com>
2021-11-06 06:50:30 -0300

    commit
    criando um projeto usando o dao

---
+++ demo-dao-jdbc/src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Departmente;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Departmente obj = new Departmente(1, "Books");
+		System.out.println(obj);
+	}
+
+}
