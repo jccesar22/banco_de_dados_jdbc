82baa752aeb81e412bb38ad771aef941d09ac092
Júlio César Soares de Almeida <60848753+jccesar22@users.noreply.github.com>
2021-11-06 07:01:24 -0300

    commit
    colocando a class seller

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departmente;
 
 public class Program {
@@ -7,7 +9,10 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Departmente obj = new Departmente(1, "Books");
-		System.out.println(obj);
+		
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		System.out.println(seller);
 	}
 
 }
