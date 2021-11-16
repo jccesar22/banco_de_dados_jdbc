952aa27b973cfa895993aaa2b0f2b805b3598f6a
Júlio César Soares de Almeida <60848753+jccesar22@users.noreply.github.com>
2021-11-16 16:27:11 -0300

    commit

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -9,6 +9,7 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SellerDao sellerDao = DaoFactory.creatSellerDao();
+		System.out.println("=== teste 1: Seller FindbyId");
 		Seller seller = sellerDao.FinfById(3);
 		
 		System.out.println(seller);
